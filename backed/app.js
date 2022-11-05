9acd3383b6d3c75b82477fc9102fe0bf9a5af4fc
obov <obov3e@gmail.com>
2022-11-05 12:25:30 +0900

    cors

--- backed/app.js
+++ backed/app.js
@@ -23,7 +23,8 @@ const options = {
 };
 const openapiSpec = swaggerJsdoc(options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpec));
-
+var cors = require("cors");
+app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  function getHello() {
+    const great = document.getElementById("greet");
+    fetch("http://localhost:4000/api/hello")
+      .then((res) => res.json())
+      .then((data) => (great.innerHTML = JSON.stringify(data)));
+  }
+  useEffect(getHello);
   return (
     <div className="App">
       <div>
@@ -21,6 +27,9 @@ function App() {
           count is {count}
         </button>
         <p>
+          api called : <code id="greet"></code>
+        </p>
+        <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
@@ -28,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
